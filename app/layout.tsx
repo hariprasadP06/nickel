f6f5d9447d5e7b5a733ba476e3876f759b9b2a90
Hari prasad P <hariprasad.1si22cs404@gmail.com>
2025-04-16 11:12:36 +0530

    chore: initialize project with package.json and tsconfig.json Added package.json with dependencies and scripts for Next.js application. - Created tsconfig.json with TypeScript compiler options and settings.

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { Theme } from "@radix-ui/themes";
+import "./globals.css";
+
+import { Metadata } from "next";
+import { PropsWithChildren } from "react";
+import { ThemeProvider } from "@/components/ThemeProvider";
+
+export const metadata: Metadata = {
+  title: "Nickel",
+  description: "A crazy element",
+};
+
+const RootLayout = (props: PropsWithChildren) => {
+  return (
+    <html suppressHydrationWarning>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Theme accentColor="indigo">{props.children}</Theme>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
